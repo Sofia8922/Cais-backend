c959fd120b64b8b0026debf6faaee71711c5ccfc
Sofia Klören <Sf.kloren@outlook.com>
2026-01-06 17:07:06 +0100

    more dummy data

--- Cáis/src/main/java/com/school/Cais/Miscellaneous/DummyData.java
+++ Cáis/src/main/java/com/school/Cais/Miscellaneous/DummyData.java
@@ -67,25 +67,35 @@ public class DummyData {
         //----------------------------------------------------
         ProductCreateDTO[] productCreateDTOS = {
             new ProductCreateDTO("Brie", "1 kg. Brie is a soft cow's-milk cheese named after Brie. The rind is typically eaten, with its flavour depending largely upon the ingredients used and its manufacturing environment.", 3.45f, 40, "broken link here", 3L),
+            new ProductCreateDTO("Bavarian Blue Cheese", "", 19.5f, 250, "https://upload.wikimedia.org/wikipedia/commons/2/24/Bavaria_blu_01_WikiCheese_Lokal_K.jpg", 3L),
             new ProductCreateDTO("Gouda", "ᛅᛚᛁᛦ᛬ᛘᛁᚾ᛬ᛁᛦᚢ᛬ᛒᚢᚱᚾᛁᛦ᛬ᚠᚱᛁᛅᛚᛋᛁᛦ᛬ᛅᚢᚴ᛬ᛁᛅᚠᚾᛁᛦ᛬ᛅᛏ᛬ᚢᛁᚱᚦᛁᚴᚢ᛬ᛅᚢᚴ᛬ᚱᛁᛏᚢᛘ᛬ᚦᛅᛁᛦ᛬ᛁᛦᚢ᛬ᛅᛚᛁᛦ᛬ᚢᛁᛏᛁ᛬ᚴᚢᛏᛁᛦ᛬ᛅᚢᚴ᛬ᛋᛅᛘᚢᛁᛋᚴᚢ᛬ᛅᚢᚴ᛬ᛋᚴᚢᛚᚢ᛬ᚴᛅᚱᛅ᛬ᚼᚢᛅᛦ᛬ᛏᛁᛚ᛬ᛅᚾᛅᚱᛋ᛬ᛒᚱᚢᚦᚢᚱᛚᛁᚴᛅ ", 10.4f, 104, "https://president-professionnel.com/wp-content/uploads/2024/02/gouda-wheel-president.png", 1L),
+            new ProductCreateDTO("Mascarpone", "", 10f, 470, "https://ichu.com.hk/wp-content/uploads/2025/02/mascarpone-cheese-recipe-1739314048.jpg", 2L),
             new ProductCreateDTO("La Vache Qui Rit", "", 4.5f, 0, "", 3L),
             new ProductCreateDTO("La Vache Qui Rit (Vegan)", "", 4.5f, 231, "https://www.lavachequirit.ca/wp-content/uploads/2024/01/plant-product-single-768x768.png", 3L),
             new ProductCreateDTO("Mozzarella", "\uD83C\uDDEE\uD83C\uDDF9 italiano \uD83C\uDDEE\uD83C\uDDF9", 4.65f, 3, "https://upload.wikimedia.org/wikipedia/commons/thumb/8/83/Three_mozzarellas_on_the_toast.jpg/1280px-Three_mozzarellas_on_the_toast.jpg", 2L),
             new ProductCreateDTO("Mozzarella Tradizionale", "", 6f, 500, "https://upload.wikimedia.org/wikipedia/commons/thumb/8/83/Three_mozzarellas_on_the_toast.jpg/1280px-Three_mozzarellas_on_the_toast.jpg", 2L),
             new ProductCreateDTO("Standard Cheese Slicer", "チーズナイフは、チーズを切ることに特化したキッチンナイフ。オメガナイフとも呼ばれる。", 20f, 1, "https://upload.wikimedia.org/wikipedia/commons/thumb/2/22/Kaasschaaf_2022.jpg/1200px-Kaasschaaf_2022.jpg", 4L),
-            new ProductCreateDTO("Edam Cheese", "0.2 kg. Edam is traditionally sold in flat-ended spheres with a pale yellow interior and a coat, or rind, of red paraffin wax. Edam ages and travels well and hardens, instead of spoiling, for an extended time.", 17.5f, 1000, "https://www.thedailymilk.nl/wp-content/uploads/2015/10/Edammer-kaas.jpg", 1L)
+            new ProductCreateDTO("Edam Cheese", "0.2 kg. Edam is traditionally sold in flat-ended spheres with a pale yellow interior and a coat, or rind, of red paraffin wax. Edam ages and travels well and hardens, instead of spoiling, for an extended time.", 17.5f, 1000, "https://www.thedailymilk.nl/wp-content/uploads/2015/10/Edammer-kaas.jpg", 1L),
+            new ProductCreateDTO("Sbinz", "Swiss cheese", 8f, 0, "https://upload.wikimedia.org/wikipedia/commons/b/b3/Sbr_product_0020_bc.jpg", 3L),
+            new ProductCreateDTO("Boerenkaas", "", 3f, 0, "https://cheeseinabox.nl/wp-content/uploads/2023/12/Boerenkaas-Jong-1.jpg", 1L),
+            new ProductCreateDTO("Emmental", "", 3f, 920, "https://upload.wikimedia.org/wikipedia/commons/3/38/Emmentaler_Premier_Cru.jpg", 3L),
+            new ProductCreateDTO("Maasdam", "Holland!", 3.5f, 0, "https://upload.wikimedia.org/wikipedia/commons/2/25/Maasdam-cheese.jpg", 1L),
+            new ProductCreateDTO("Old Amsterdam", "", 1.9f, 0, "https://upload.wikimedia.org/wikipedia/commons/1/16/Vintage_Old_Amsterdam_cheese%2C_Amsterdam_%282023%29_01.jpg", 1L),
+            new ProductCreateDTO("Harter Käse", "From Germany \uD83C\uDDE9\uD83C\uDDEA", 2.4f, 0, "https://upload.wikimedia.org/wikipedia/commons/0/08/Harzer_K%C3%A4se_12_WikiCheese_Lokal_K.jpg", 3L),
+            new ProductCreateDTO("Smoked Austrian", "Not From Germany \uD83C\uDDE9\uD83C\uDDEA", 3f, 921, "https://upload.wikimedia.org/wikipedia/commons/b/b9/Smoked_Austrian_cheese.jpg", 3L),
+            new ProductCreateDTO("Schimmelkäse", "Austrian", 5f, 401, "https://upload.wikimedia.org/wikipedia/commons/8/86/Gr%C3%BCnschimmelk%C3%A4se_%C3%96sterkron.jpg", 3L),
         };
         for(ProductCreateDTO pcd : productCreateDTOS) {
             productController.createProduct(pcd);
         }
         //---------------------------------------------------
         cartInfo[] cartInfos = {
-            new cartInfo(1L, 2L, 1),
-            new cartInfo(1L, 8L, 13),
-            new cartInfo(2L, 1L, 1),
-            new cartInfo(2L, 5L, 3),
-            new cartInfo(2L, 8L, 50),
-            new cartInfo(3L, 8L, 20)
+            new cartInfo(1L, 3L, 1),
+            new cartInfo(1L, 10L, 13),
+            new cartInfo(2L, 2L, 1),
+            new cartInfo(2L, 7L, 3),
+            new cartInfo(2L, 10L, 50),
+            new cartInfo(3L, 10L, 20)
         };
         for(cartInfo ci : cartInfos) {
             accountController.addToCart(ci.acc, ci.prod, ci.amount);
@@ -95,12 +105,12 @@ public class DummyData {
             accountController.checkout(id);
         }
         cartInfo[] cartInfosNotSold = {
-           new cartInfo(1L, 2L, 5),
-           new cartInfo(2L, 6L, 2),
-           new cartInfo(2L, 8L, 15),
-           new cartInfo(3L, 7L, 1),
-           new cartInfo(3L, 1L, 1),
-           new cartInfo(3L, 8L, 20)
+           new cartInfo(1L, 3L, 5),
+           new cartInfo(2L, 8L, 2),
+           new cartInfo(2L, 10L, 15),
+           new cartInfo(3L, 9L, 1),
+           new cartInfo(3L, 2L, 1),
+           new cartInfo(3L, 10L, 20)
         };
         for(cartInfo ci : cartInfosNotSold) {
             accountController.addToCart(ci.acc, ci.prod, ci.amount);

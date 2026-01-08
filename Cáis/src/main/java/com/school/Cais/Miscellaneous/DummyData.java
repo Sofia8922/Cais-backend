229e70d851bb88402e43df54a1e0f3c840c15110
Sofia Klören <Sf.kloren@outlook.com>
2026-01-08 11:40:48 +0100

    more dummy data

--- Cáis/src/main/java/com/school/Cais/Miscellaneous/DummyData.java
+++ Cáis/src/main/java/com/school/Cais/Miscellaneous/DummyData.java
@@ -45,7 +45,7 @@ public class DummyData {
         AccountRegisterDTO[] accountRegisterDTOs = {
             new AccountRegisterDTO("Alice", Constants.Password, Constants.UserEmail, List.of(Role.ADMIN)),
             new AccountRegisterDTO("Brandon", Constants.Password, Constants.UserEmail, List.of(Role.USER)),
-            new AccountRegisterDTO("Chloë", Constants.Password, Constants.UserEmail, List.of(Role.USER))
+            new AccountRegisterDTO("Chloë", Constants.Password, "fake@email.ru", List.of(Role.USER))
         };
         for(AccountRegisterDTO ard : accountRegisterDTOs) {
             accountController.register(ard);
